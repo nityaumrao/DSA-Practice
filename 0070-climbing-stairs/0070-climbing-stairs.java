7e4f7a5e0d6eb7458c6d8e546f3d97565b39bfe3
NITYA UMRAO <184013981+nityaumrao@users.noreply.github.com>
2026-07-24 19:25:43 +0530

    Time: 0 ms (100%), Space: 42.2 MB (36.07%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,15 +1,13 @@
 class Solution {
     public int climbStairs(int n) {
         int [] dp = new int[n+1];
-        Arrays.fill(dp,-1);
-        dp[0]=0;
-        dp[1]=1;
         return f1(n,dp);
 
     }
     private int f1(int n , int[] dp){
-        if(n==0) return 1 ;
-        if(dp[n]!= -1) return dp[n];
+        if(n==0 || n == 1) return 1 ;
+        if(dp[n]!= 0) return dp[n];
+
         dp[n] = f1(n-1, dp) + f1(n-2,dp);
         return dp[n];
     }
