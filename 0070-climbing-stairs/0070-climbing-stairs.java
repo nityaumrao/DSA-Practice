973a07d1dd7df6ea9340612a16caaabfa26675de
NITYA UMRAO <184013981+nityaumrao@users.noreply.github.com>
2026-07-25 21:40:16 +0530

    Time: 0 ms (100%), Space: 41.9 MB (73.07%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -2,7 +2,6 @@ class Solution {
     public int climbStairs(int n) {
         int [] dp = new int[n+1];
         return f1(n,dp);
-
     }
     private int f1(int n , int[] dp){
         if(n==0 || n == 1) return 1 ;
