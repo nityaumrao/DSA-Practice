8e6448054a498570791b45db5fe3e8f9fc18ffe5
NITYA UMRAO <184013981+nityaumrao@users.noreply.github.com>
2026-07-30 05:50:12 +0530

    Time: 3 ms (58.63%), Space: 44.9 MB (40.97%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,17 +1,19 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        s=s.toLowerCase().replaceAll("[^a-z0-9]","");
-        char [] chars = s.toCharArray();
-        int left=0;
-        int right=chars.length-1;
-        while(left<right){
-            if(chars[left]!=chars[right]){
-                return false ;
+        int n = s.length();
+        StringBuilder sB = new StringBuilder();
+        for(int i = 0 ; i<n ; i++){
+            char ch = s.charAt(i);
+            if(Character.isLetterOrDigit(ch)) {
+                sB.append(Character.toLowerCase(ch));
             }
-            left++;
-            right--;
+        }
+        n= sB.length();
+        for(int i = 0 ; i<n/2 ; i++){
+            char chF = sB.charAt(i);
+            char chL = sB.charAt(n-i-1);
+            if(chF != chL) return false ;
         }
         return true ;
-        
     }
 }
