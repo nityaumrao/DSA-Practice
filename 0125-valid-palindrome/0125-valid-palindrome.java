5cdd0b7fe9e16a15e73c7d98b62c4330115c5a49
NITYA UMRAO <184013981+nityaumrao@users.noreply.github.com>
2026-07-30 06:03:28 +0530

    Time: 2 ms (99.33%), Space: 44.3 MB (64.77%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -2,17 +2,20 @@ class Solution {
     public boolean isPalindrome(String s) {
         int n = s.length();
         StringBuilder sB = new StringBuilder();
-        for(int i = 0 ; i<n ; i++){
-            char ch = s.charAt(i);
-            if(Character.isLetterOrDigit(ch)) {
-                sB.append(Character.toLowerCase(ch));
+        int i = 0 , j = n-1 ; 
+        while(i<j){
+            char chL = s.charAt(i);
+            char chR = s.charAt(j);
+            if(!Character.isLetterOrDigit(chL)) {
+                i++;
+            }else if(!Character.isLetterOrDigit(chR)){
+                j--;
+            }else if(Character.toLowerCase(chR)!=Character.toLowerCase(chL)){
+                return false ;
+            }else {
+                i++;
+                j--;
             }
-        }
-        n= sB.length();
-        for(int i = 0 ; i<n/2 ; i++){
-            char chF = sB.charAt(i);
-            char chL = sB.charAt(n-i-1);
-            if(chF != chL) return false ;
         }
         return true ;
     }
