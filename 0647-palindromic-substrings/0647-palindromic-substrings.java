4b85b92f2e00aa06e3a00be5a58b252a397c7509
NITYA UMRAO <184013981+nityaumrao@users.noreply.github.com>
2026-08-04 18:43:03 +0530

    Time: 980 ms (5.02%), Space: 46.6 MB (22.12%) - LeetHub

---
+++ 0647-palindromic-substrings/0647-palindromic-substrings.java
@@ -0,0 +1,33 @@
+class Solution {
+    int count ;
+    public int countSubstrings(String s) {
+        
+        int n = s.length();
+        for(int i = 0 ; i<n ; i++){
+            for(int j = i ; j<n ; j++){
+                if (f1(s.substring(i,j+1))) count++;
+            }
+        }
+            
+        return count ;
+    }
+    private void f2(String s ){
+        // if(s.length()==0) return ;
+        // if(s.length()==1){
+        //     count++;
+        //     return ;
+        // }
+        
+        // f2(s.substring(i+1,j+1));
+        // f2(s.substring(i,j));
+
+    }
+    private boolean f1(String s ){
+        if(s.length()==0) return false ;
+        int n = s.length();
+        for(int i = 0 ; i<n/2 ; i++){
+            if(s.charAt(i)!=s.charAt(n-1-i)) return false ;
+        }
+        return true ;
+    }
+}
